8c92f7b9330f1290a2613fd778746e1a50b2d432
Your Name <you@example.com>
2025-03-13 14:32:23 +0530

    product

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import CustomUser,Category,Product
+from .models import CustomUser,Category,Product,Order, OrderItem
+ 
 
 class CustomUserAdmin(UserAdmin):
     # Specify the fields to be displayed in the user list
@@ -35,6 +36,38 @@ class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'short_description', 'price', 'available_quantity', 'sale_price', 'category')  # Customize displayed fields
     search_fields = ('name', 'category__name')  # Allow searching by product name and category name
     list_filter = ('category',)  # Filter products by category
+
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1  # Number of empty forms to display by default
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'total_price', 'status', 'payment_status', 'created_at']
+    list_filter = ['status', 'payment_status', 'created_at']
+    search_fields = ['user__username', 'status', 'payment_status']
+    inlines = [OrderItemInline]  # Include order items inline in the order view
+
+    # Optional: Add actions for bulk update
+    actions = ['mark_as_shipped', 'mark_as_delivered', 'mark_as_cancelled']
+
+    def mark_as_shipped(self, request, queryset):
+        queryset.update(status='shipped')
+    mark_as_shipped.short_description = "Mark selected orders as shipped"
+
+    def mark_as_delivered(self, request, queryset):
+        queryset.update(status='delivered')
+    mark_as_delivered.short_description = "Mark selected orders as delivered"
+
+    def mark_as_cancelled(self, request, queryset):
+        queryset.update(status='cancelled')
+    mark_as_cancelled.short_description = "Mark selected orders as cancelled"
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(OrderItem)
+
 
 admin.site.register(Product, ProductAdmin)
 
